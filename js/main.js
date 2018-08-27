b64dec4dcfd25fb8d6380cdf828fc540872864a7
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-08-27 22:43:06 +0200

    Updated main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 // Here we instanciate the different classes.
-
 const crypto = new cryptopricesClass();
 const ui = new interfaceClass();
+
+// Next we get the form.
+const form = document.getElementById('form');
+// Set an event listener when we send the form.
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Next we read the selected currency.
+  const coinSelect = document.getElementById('currency');
+  const selectedCoin = coinSelect.options[coinSelect.selectedIndex].value;
+
+  // Next we read the selected cryptocurrency.
+  const cryptoCurrencySelect = document.getElementById('criptocurrency');
+  const cryptoSelectedCoin = cryptoCurrencySelect.options[cryptoCurrencySelect.selectedIndex].value;
+
+  // Here we check that both fields have data.
+  if(selectedCoin === '' || cryptoSelectedCoin === '') {
+    console.log('We need details...');
+  } else {
+    console.log('Get price...');
+  }
+});
+
 
